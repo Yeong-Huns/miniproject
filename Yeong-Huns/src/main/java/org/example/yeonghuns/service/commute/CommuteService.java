97727fc2214345bb753419a9eb9531862f8399e4
yeoung_huns <vosxja1@gmail.com>
2024-03-08 04:47:28 +0900

    Step03

--- Yeong-Huns/src/main/java/org/example/yeonghuns/service/commute/CommuteService.java
+++ Yeong-Huns/src/main/java/org/example/yeonghuns/service/commute/CommuteService.java
@@ -2,10 +2,8 @@ package org.example.yeonghuns.service.commute;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.yeonghuns.config.Error.exception.commute.AbsentCheckOutException;
-import org.example.yeonghuns.config.Error.exception.commute.AlreadyAttendanceException;
-import org.example.yeonghuns.config.Error.exception.commute.AlreadyDepartureException;
-import org.example.yeonghuns.config.Error.exception.commute.CommuteNotFoundException;
+import org.example.yeonghuns.config.Error.exception.commute.*;
+import org.example.yeonghuns.domain.AnnualLeave;
 import org.example.yeonghuns.domain.Commute;
 import org.example.yeonghuns.domain.Member;
 import org.example.yeonghuns.dto.commute.request.EndOfWorkRequest;
@@ -14,13 +12,16 @@ import org.example.yeonghuns.dto.commute.request.StartOfWorkRequest;
 import org.example.yeonghuns.dto.commute.response.GetCommuteDetail;
 import org.example.yeonghuns.dto.commute.response.GetCommuteRecordResponse;
 import org.example.yeonghuns.repository.CommuteRepository;
+import org.example.yeonghuns.service.annual.AnnualLeaveService;
 import org.example.yeonghuns.service.member.MemberService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * packageName    : org.example.yeonghuns.service.commute
@@ -38,18 +39,21 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CommuteService {
     private final CommuteRepository commuteRepository;
+    private final AnnualLeaveService annualLeaveService;
     private final MemberService memberService;
 
     @Transactional
     public void startOfWork(StartOfWorkRequest request) {
         Member member = memberService.findMemberById(request.id());
         Optional<Commute> commute = findLatestCommuteByMember(member);
-        if(commute.isPresent()) {
+        if (commute.isPresent()) {
             Commute latestCommute = commute.orElseThrow(CommuteNotFoundException::new);
             if (latestCommute.isAttendance()) throw new AbsentCheckOutException(); //이전 기록 퇴근확인
             boolean isAlreadyAttendance = LocalDate.now().equals(LocalDate.from(latestCommute.getCreatedAt()));
             if (isAlreadyAttendance) throw new AlreadyAttendanceException(); //당일 출근기록 확인
         }
+        boolean isAlreadyAnnualLeaves = annualLeaveService.isAlreadyUsingAnnualLeaves(member, LocalDate.now());
+        if (isAlreadyAnnualLeaves) throw new UsingAnnualLeavesException(); //인권보장
         commuteRepository.save(request.toEntity(member));
     }
 
@@ -77,17 +81,33 @@ public class CommuteService {
     }
 
 
-
     private Optional<Commute> findLatestCommuteByMember(Member member) {
         return commuteRepository.findFirstByMemberIdOrderByCreatedAtDesc(member.getId());
     }
 
     private List<GetCommuteDetail> findCommuteListByMemberIdAndStartOfWork(GetCommuteRecordRequest request) {
-        List<Commute> commuteList =
-                commuteRepository.findCommuteListByMemberIdAndStartOfWork(request.id(), request.yearMonth().getYear(), request.yearMonth().getMonth().getValue());
+        List<Commute> commuteList = commuteRepository
+                .findCommuteListByMemberIdAndStartOfWork(request.id(), request.getYear(), request.getMonth());
+        if (commuteList.isEmpty()) throw new CommuteNotFoundException();
+        //해당범위에 통근기록 존재 X? -> 통근기록없음 예외처리
+        List<GetCommuteDetail> commuteDetailList = commuteList.stream()
+                .map(GetCommuteDetail::from)
+                .collect(Collectors.toList()); //통근기록을 CommuteDetail으로 변환
 
-        if (commuteList.isEmpty()) throw new CommuteNotFoundException(); //해당범위에 통근기록 존재 X
+        List<AnnualLeave> annualLeaveLeavesList = annualLeaveService // 연차기록찾기 (오늘보다 미래의 연차기록은 가져오지않음)
+                .findAnnualLeavesByMemberIdAndYearMonth(request.id(), request.yearMonth());
 
-        return commuteList.stream().map(GetCommuteDetail::from).toList(); //CommuteDetail으로 변환
+        mergeAndSort(commuteDetailList, annualLeaveLeavesList); //
+        return commuteDetailList;
+    }
+
+    private void mergeAndSort(List<GetCommuteDetail> commuteDetailList, List<AnnualLeave> annualLeaveLeavesList) {
+        if (annualLeaveLeavesList != null) { //해당범위 연차기록이 있으면 합침
+            List<GetCommuteDetail> annualLeavesToDetails = annualLeaveLeavesList.stream()
+                    .map(GetCommuteDetail::from)
+                    .toList();
+            commuteDetailList.addAll(annualLeavesToDetails);
+        }
+        commuteDetailList.sort(Comparator.comparing(GetCommuteDetail::date));
     }
 }
